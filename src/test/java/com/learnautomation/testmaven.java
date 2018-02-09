d3397a5f023f79f887bfe1443f3dee8bf4c28b39
user <user@DESKTOP-5EESGLJ>
2018-02-09 16:07:11 +0530

    added

---
+++ src/test/java/com/learnautomation/testmaven.java
@@ -0,0 +1,13 @@
+package com.learnautomation;
+
+import org.testng.annotations.Test;
+
+public class testmaven 
+{
+
+	@Test
+	public void test1()
+	{
+		System.out.println("welcome to selenium world");
+	}
+}
